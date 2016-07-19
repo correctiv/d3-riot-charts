cba9645c3538741fd216cd84cecfe4876ebd706f
Simon Wörpel <simon.woerpel@medienrevolte.de>
2016-07-19 11:14:00 +0200

    update docstring in `chart.js`

--- src/chart.js
+++ src/chart.js
@@ -20,6 +20,8 @@ import ChartStore from './stores/chart_store.js'
 /**
  * available opts:
  *
+ * see `playbooks/defaults.js` for defaults
+ *
  * @param {string} elementId - id of html element,
  *    if this element doesn't exist, it will be created
  * @param {string} kind - kind of chart,
@@ -27,10 +29,31 @@ import ChartStore from './stores/chart_store.js'
  * @param {string} dataUrl - url to csv file, will be loaded via `PapaParse`
  * @param {number} width - overall width of chart svg
  * @param {number} height - overall height of chart svg
+ * @param {number} wrapperWidth - width of wrapper element, default at least `width`
+ * @param {number} wrapperHeight - height of wrapper element, default at least `height`
+ * @param {string} wrapperClass - css class for wrapper element
+ * @param {object} margin - margins for svg chart element
  * @param {string} xCol - csv column for x-Axis
  * @param {string} yCol - csv column for y-Axis
+ * @param {boolean} showXAxis - whether to render x-Axis or not
+ * @param {boolean} showYAxis - whether to render y-Axis or not
+ * @param {boolean} xScaleNice - whether to call `.nice()` on xScale or not
+ * @param {boolean} yScaleNice - whether to call `.nice()` on yScale or not
+ * @param {boolean} responsive - whether to resize chart on window/container resize or not
+ * @param {boolean} responsiveSvg - whether to use `preserveAspectRatio` on svg or not
+ * @param {number} xTicks - how many ticks the x-Axis should have (reduced on smaller windows)
+ * @param {number} yTicks - how many ticks the y-Axis should have (reduced on smaller windows)
+ * @param {object, function, array, string} color - a color function, mapping, array or a single string value
+ * @param {function} filter - a filter function that will be applied to each row while loading data
  * @param {string} sizeCol - csv column for calculating dot size (for `scatterChart`)
- * @param {object} tooltip – `headTempl` or `labelCol` and optional `bodyTempl` for tooltip rendering
+ * @param {object} or {boolean} tooltip - `headTempl` or `labelCol` and optional `bodyTempl` for tooltip rendering
+ * @param {object} or {boolean} search - search settings, see examples
+ * @param {object} or {boolean} legend - legend settings, see examples
+ * @param {boolean} clearSvg - whether to add `clear` event for clicking on free svg space or not
+ * @param {function} drawExtra - function or array of functions to apply on the svg to draw some extra stuff
+ * @param {array} sizeRange - for scatters: smallest and biggest circle radius
+ * @param {number} size - for scatters: fixed circle radius
+ * @param {object} playbook - overrides for playbook for this chart
  *
  **/
 export default class {
